c600e45fe11ce5cdc19648902b187d40cd33e051
SynnK <93355393+synnkfps@users.noreply.github.com>
2023-10-06 17:23:51 -0300

    Update sketch.js

--- Ping Pong/sketch.js
+++ Ping Pong/sketch.js
@@ -5,6 +5,7 @@ let size = 20;
 
 let base = 6;
 
+// posições bolinha
 let x;
 let y;
 
@@ -19,7 +20,7 @@ let raquetePlayerSpeed = 5;
 let raqueteOponenteX = 560;
 let raqueteOponenteY;
 let raqueteOponenteSize = [10, 105];
-let raqueteOponenteSpeed = 5;
+let raqueteOponenteSpeed = 3;
 
 // Placar
 let pontosPlayer = 0;
@@ -40,44 +41,52 @@ function setup() {
 
 function draw() {
   background(0);
-  gerarBackground()
-  
-  fill(255, 106, 0)
+  gerarBackground();
+
   criaBolinha();
   moverBolinha();
 
-  fill(50, 102, 168)
-  criaRaquete(raquetePlayerX, raquetePlayerY, raquetePlayerSize[0], raquetePlayerSize[1]);
-  fill(168, 50, 50)
-  criaRaquete(raqueteOponenteX, raqueteOponenteY, raqueteOponenteSize[0], raqueteOponenteSize[1]);
+  criaRaquete(raquetePlayerX, raquetePlayerY, raquetePlayerSize[0], raquetePlayerSize[1], true);
+  criaRaquete(raqueteOponenteX, raqueteOponenteY, raqueteOponenteSize[0], raqueteOponenteSize[1], false);
 
   ouveRaquete(raquetePlayerY, raquetePlayerSize[1]);
   moveRaqueteOponente();
 
   textSize(30);
-  fill(50, 102, 168)
-  text(pontosPlayer, width / 2 - 60, 30);
-  fill(168, 50, 50)
-  text(pontosOponente, width / 2 + 60, 30);
-  
+
+  criaPlacar(true);
+  criaPlacar(false);
 }
 
-function criaRaquete(x, y, w, h) {
+function criaPlacar(player) {
+  if (player) {
+    fill(50, 102, 168)
+    text(pontosPlayer, width / 2 - 60, 30);
+  } else {
+    fill(168, 50, 50)
+    text(pontosOponente, width / 2 + 60, 30);
+  }
+}
+
+function criaRaquete(x, y, w, h, isPlayer) {
+  if (isPlayer) {
+    fill(50, 102, 168)
+  } else {
+    fill(168, 50, 50)
+  }
   rect(x, y, w, h);
 }
 
 function ouveRaquete(raqueteY, raqueteHeight) {
+  movendoInverso = false;
+
   if (keyIsDown(UP_ARROW) && raqueteY >= 0) {
     raquetePlayerY -= raquetePlayerSpeed;
     movendoInverso = !movendoInverso
-  } else {
-    movendoInverso = false;
   }
   if (keyIsDown(DOWN_ARROW) && raqueteY + raqueteHeight <= height) {
     raquetePlayerY += raquetePlayerSpeed;
     movendoInverso = !movendoInverso
-  } else {
-    movendoInverso = false;
   }
 }
 
@@ -90,12 +99,13 @@ function moveRaqueteOponente() {
 }
 
 function criaBolinha() {
+  fill(255, 106, 0)
   circle(x, y, size);
 }
 
 function moverBolinha() {
-  if (x + size >= width || x - size <= 0) speedX *= -1;
-  if (y + size >= height || y - size <= 0) speedY *= -1;
+  if (x + size >= width || x - size <= 0) { speedX *= -1;}
+  if (y + size >= height || y - size <= 0) {speedY *= -1;}
 
   if (x - size <= 0) {
     pontosOponente += 1;
@@ -119,18 +129,28 @@ function resetBolinha() {
 }
 
 function colidiu(raqueteX, raqueteY, raqueteWidth, raqueteHeight) {
-  if (x - size < raqueteX + raqueteWidth && 
-      x + size > raqueteX && 
-      y - size < raqueteY + raqueteHeight && 
-      y + size > raqueteY) {
-    if (movendoInverso) speedY *= -1
+  // Colisão com a raquete do jogador
+  if (
+    x - size <= raqueteX + raqueteWidth &&
+    x + size >= raqueteX &&
+    y >= raqueteY &&
+    y <= raqueteY + raqueteHeight
+  ) {
     speedX *= -1;
   }
 }
 
+
 function gerarBackground() {
-  fill(87, 87, 87)
-  for (let i=0; i < height; i+=90) {
-    rect(width/2, i, 9, 30)
+  fill(12, 15, 36)
+  rect(0,0,width,height)
+
+  fill(255)
+  rect(0, height/2-1, width, 2);
+
+  fill(255)
+  for (let i=0; i < height; i+=61.5) {
+    rect(width/2 - 4.5, i, 9, 30)
   }
+
 }
