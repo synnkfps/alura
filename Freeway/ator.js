99976f323d6a1163adc1e9cad58b3970d9a14ef7
SynnK <93355393+synnkfps@users.noreply.github.com>
2023-10-20 22:53:43 -0300

    Update ator.js

--- Freeway/ator.js
+++ Freeway/ator.js
@@ -5,11 +5,15 @@ function mostraAtor(){
   image(imagemDoAtor, 100, yAtor, 30, 30);
 }
 
-function movimentaAtor(){
-  if (keyIsDown(UP_ARROW)){
+function movimentaAtor() {
+  if (yAtor <= 10) {
+    pontuacao += 1;
+    yAtor = 366
+  }
+  if (keyIsDown(UP_ARROW) && yAtor > 0) {
     yAtor -= 3;
   }
-  if (keyIsDown(DOWN_ARROW)){
+  if (keyIsDown(DOWN_ARROW) && yAtor+30<height) {
     yAtor += 3;
   }
 }
